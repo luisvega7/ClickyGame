100fecdda10428d2751e6e2218da0d9eb69b4b3e
Luis Vega <44487505+luisvega7@users.noreply.github.com>
2019-03-30 00:00:36 -0700

    Add files via upload

---
+++ src/components/Header/index.js
@@ -0,0 +1,13 @@
+import React from "react";
+import "./style.css";
+
+function Header() {
+  return (
+    <header className="header">
+      <h1>Dragon Ball Z Budokai!</h1>
+      <h2>Click on an image of your favorite DBZ characters ,but warning! Do not click on the same one. </h2>
+    </header>
+  );
+}
+
+export default Header;
